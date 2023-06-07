07de35d2904fd9cfb122c2f197584bce95ef0e59
AGhafoerkhan <112637552+AGhafoerkhan@users.noreply.github.com>
2023-06-07 16:57:57 +0200

    Quality of life fixes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const overlayButton = document.querySelector(".overlay-button");
-  const overlay = document.querySelector(".overlay");
-  const overlayContent = document.querySelector(".overlay-content");
-  let isOpen = false;
+const overlayButton = document.querySelector(".overlay-button");
+const overlay = document.querySelector(".overlay");
+const overlayContent = document.querySelector(".overlay-content");
+const body = document.querySelector("body");
 
-  overlayButton.addEventListener("click", function () {
-    isOpen = !isOpen;
-    if (isOpen) {
-      overlay.style.display = "block";
-      overlayContent.style.display = "flex"; // Show the subjects
-      overlay.classList.remove("fade-out");
-      overlay.classList.add("fade-in");
-    } else {
-      overlayContent.style.display = "none"; // Hide the subjects
-      overlay.classList.add("fade-out");
-      overlay.classList.remove("fade-in");
-    }
-  });
+let isOpen = false;
+
+overlayButton.addEventListener("click", function () {
+  isOpen = !isOpen;
+  if (isOpen) {
+    overlay.style.display = "block";
+    overlayContent.style.display = "flex"; // Show the subjects
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+    body.style.overflow = "hidden";
+  } else {
+    overlayContent.style.display = "none"; // Hide the subjects
+    overlay.classList.add("fade-out");
+    overlay.classList.remove("fade-in");
+    body.style.overflow = "auto";
+  }
 });
