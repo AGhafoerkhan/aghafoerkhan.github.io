1887fd2f81c3e025431f1feddfa688a8f6a8a5cb
AGhafoerkhan <112637552+AGhafoerkhan@users.noreply.github.com>
2023-06-16 11:43:11 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -32,6 +32,65 @@ function fadeInOut() {
   }
 };
 
+function updateLastUpdatedMessage() {
+  let now = new Date();
+  now.setTime(now.getTime() - (8 * 60 * 60 * 1000)); // Subtracting 8 hours in milliseconds
+
+  let options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric' };
+  let message = "Last updated: " + now.toLocaleString('en-US', options);
+
+  let elements = document.querySelectorAll(".last-updated");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].textContent = message;
+  }
+}
+
+updateLastUpdatedMessage();
+
+var texts = ['W', 'e', 'l', 'c', 'o', 'm', 'e', ': )'];
+var number_of_particle = 12;
+
+for (var i = 0; i < texts.length; i++) {
+  var backgroundClass = "background" + i;
+  // Create background element with the specified class
+  var backgroundElement = document.createElement('div');
+  backgroundElement.classList.add(backgroundClass);
+  document.body.appendChild(backgroundElement);
+
+  // Create criterion element
+  var criterionElement = document.createElement('div');
+  criterionElement.classList.add('criterion');
+
+  // Create text elements
+  for (var j = 0; j < texts.length; j++) {
+    var textClass = 'text' + j;
+    var textElement = document.createElement('div');
+    textElement.classList.add(textClass);
+    textElement.innerText = texts[j];
+    criterionElement.appendChild(textElement);
+  }
+
+  // Create frame elements
+  for (var k = 0; k < texts.length; k++) {
+    var frameClass = 'frame' + k;
+    var frameElement = document.createElement('div');
+    frameElement.classList.add(frameClass);
+    criterionElement.appendChild(frameElement);
+  }
+
+  // Create particle elements
+  for (var l = 0; l < texts.length; l++) {
+    for (var m = 0; m < number_of_particle; m++) {
+      var particleClass = "particle" + l + m;
+      var particleElement = document.createElement('div');
+      particleElement.classList.add(particleClass);
+      criterionElement.appendChild(particleElement);
+    }
+  }
+
+  document.body.appendChild(criterionElement);
+}
+
 
 
 
